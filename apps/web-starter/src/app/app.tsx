e774a99c28b86e69162be0cd49f0677518f7d8f8
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-15 15:47:51 -0700

    Add links and settings page

--- apps/web-starter/src/app/app.tsx
+++ apps/web-starter/src/app/app.tsx
@@ -1,13 +1,69 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { SettingsPage } from './settings-page';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="web-starter" />
-      <div />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              <ul>
+                <li>
+                  <Link to={'settings'}>Go to Settings</Link>
+                </li>
+
+                <li>
+                  <Link to={'unknown'}>Go to Unknown Page</Link>
+                </li>
+              </ul>
+
+              <NxWelcome title="web-starter" />
+            </>
+          }
+        />
+
+        <Route
+          path={'settings'}
+          element={
+            <>
+              <ul>
+                <li>
+                  <Link to={'/'}>Go Home</Link>
+                </li>
+
+                <li>
+                  <Link to={'unknown'}>Go to Unknown Page</Link>
+                </li>
+              </ul>
+
+              <SettingsPage />
+            </>
+          }
+        />
+
+        <Route
+          path={'*'}
+          element={
+            <>
+              <ul>
+                <li>
+                  <Link to={'/'}>Go Home</Link>
+                </li>
+
+                <li>
+                  <Link to={'settings'}>Go to Settings</Link>
+                </li>
+              </ul>
+
+              <div>Not found</div>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ apps/web-starter/src/app/settings-page.tsx
@@ -0,0 +1,8 @@
+export const SettingsPage = () => {
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>Welcome to the settings page!</p>
+    </div>
+  );
+};
