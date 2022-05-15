866c6fd202eafeae22bc260a97e8adc1be29499c
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-14 20:12:41 -0700

    Add API Gateway

--- apps/svc-starter/infra/main.tf
+++ apps/svc-starter/infra/main.tf
@@ -41,7 +41,7 @@ resource "aws_dynamodb_table" "primary_table" {
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "${var.app_name}.${var.environment_name}.lambda-policy"
 
-  role = aws_iam_role.lambda_role.id
+  role = aws_iam_role.lambda_exec.id
 
   policy = jsonencode({
     Version   = "2012-10-17",
@@ -60,7 +60,7 @@ resource "aws_iam_role_policy" "lambda_policy" {
 }
 
 
-resource "aws_iam_role" "lambda_role" {
+resource "aws_iam_role" "lambda_exec" {
   name = "${var.app_name}.${var.environment_name}.lambda-role"
 
   assume_role_policy = jsonencode({
@@ -76,6 +76,11 @@ resource "aws_iam_role" "lambda_role" {
       }
     ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 #endregion
@@ -95,7 +100,7 @@ resource "aws_lambda_function" "svc_function" {
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
-  role = aws_iam_role.lambda_role.arn
+  role = aws_iam_role.lambda_exec.arn
 
   runtime = var.lambda_runtime
 
@@ -104,6 +109,73 @@ resource "aws_lambda_function" "svc_function" {
       DYNAMO_TABLE_NAME = aws_dynamodb_table.primary_table.name
     }
   }
+}
+
+#endregion
+
+#region API Gateway
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "${var.app_name}.${var.environment_name}.http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "http_api_default" {
+  api_id = aws_apigatewayv2_api.http_api.id
+
+  name        = "$default"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.http_api.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+    })
+  }
+}
+
+resource "aws_cloudwatch_log_group" "http_api" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.http_api.name}"
+
+  retention_in_days = 30
+}
+
+#endregion
+
+#region API Gateway Route/Integration
+
+resource "aws_apigatewayv2_integration" "svc_function" {
+  api_id = aws_apigatewayv2_api.http_api.id
+
+  integration_uri    = aws_lambda_function.svc_function.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "svc_function" {
+  api_id = aws_apigatewayv2_api.http_api.id
+
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.svc_function.id}"
+}
+
+resource "aws_lambda_permission" "api_gw_svc_function" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.svc_function.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
 
 #endregion
