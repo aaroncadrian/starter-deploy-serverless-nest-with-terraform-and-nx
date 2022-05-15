0f52cd7f1474b63a915d0442b8482f90957296d9
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-14 19:59:22 -0700

    Add dynamo table

--- apps/svc-starter/infra/main.tf
+++ apps/svc-starter/infra/main.tf
@@ -12,3 +12,26 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+#region DynamoDB
+
+resource "aws_dynamodb_table" "primary_table" {
+  hash_key  = "pk"
+  range_key = "sk"
+
+  name = "${var.app_name}.${var.environment_name}"
+
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+}
+
+#endregion
