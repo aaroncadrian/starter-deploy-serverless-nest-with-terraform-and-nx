64a9bfbfc6b1f8edb41cecc026c787bf9d00d08d
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-15 13:16:53 -0700

    Customize error responses

--- apps/web-starter/infra/main.tf
+++ apps/web-starter/infra/main.tf
@@ -123,6 +123,18 @@ resource "aws_cloudfront_distribution" "site" {
     #    ssl_support_method       = "sni-only"
     #    minimum_protocol_version = "TLSv1.2_2018"
   }
+
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
 }
 
 #endregion
