7199894761c1049bb4b8d8241a0350051b2adc40
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-15 15:41:38 -0700

    Add mime.json

--- apps/web-starter/infra/main.tf
+++ apps/web-starter/infra/main.tf
@@ -24,13 +24,7 @@ locals {
 
   # Source: https://engineering.statefarm.com/blog/terraform-s3-upload-with-mime/
   # Source: https://dev.to/aws-builders/build-a-static-website-using-s3-route-53-with-terraform-1ele
-  mime_types = {
-    ".html" = "text/html"
-    ".js"   = "application/javascript"
-    ".css"  = "text/css"
-    ".txt"  = "text/plain"
-    ".ico"  = "image/x-icon"
-  }
+  mime_types = jsondecode(file("${path.module}/data/mime.json"))
 }
 
 #region S3 Bucket
