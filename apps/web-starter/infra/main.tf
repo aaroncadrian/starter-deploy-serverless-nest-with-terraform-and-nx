ab0ecffbda09627d3ea41d357cf2c441758be762
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-15 12:59:55 -0700

    Use default certificate

--- apps/web-starter/infra/main.tf
+++ apps/web-starter/infra/main.tf
@@ -129,7 +129,9 @@ resource "aws_cloudfront_distribution" "site" {
   }
 
   viewer_certificate {
-
+    cloudfront_default_certificate = true
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
   }
 }
 
