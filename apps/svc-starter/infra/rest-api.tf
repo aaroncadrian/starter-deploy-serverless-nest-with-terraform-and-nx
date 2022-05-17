8fb2a78049637e6258b8884a664eb23963c69f11
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-17 16:08:28 -0700

    Add only proxy

--- apps/svc-starter/infra/rest-api.tf
+++ apps/svc-starter/infra/rest-api.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_rest_api" "rest_api" {
-  name        = "${var.app_name}.${var.environment_name}.rest-api-2"
+  name        = "${var.app_name}.${var.environment_name}.rest-api"
   description = "An API for demonstrating CORS-enabled methods."
 }
 
@@ -7,6 +7,15 @@ resource "aws_api_gateway_rest_api" "rest_api" {
 
 resource "aws_api_gateway_deployment" "default" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+
+
+  triggers = {
+    redeployment = sha1(jsonencode(["testing2"]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_api_gateway_stage" "default" {
@@ -36,7 +45,7 @@ resource "aws_api_gateway_method" "proxy" {
 
 resource "aws_api_gateway_integration" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_resource.proxy_resource.id
+  resource_id = aws_api_gateway_method.proxy.resource_id
   http_method = aws_api_gateway_method.proxy.http_method
 
   type                    = "AWS_PROXY"
@@ -48,61 +57,61 @@ resource "aws_api_gateway_integration" "proxy" {
 #endregion
 
 #region CORS Method
-
-resource "aws_api_gateway_method" "proxy_cors" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_resource.proxy_resource.id
-
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-
-
-resource "aws_api_gateway_integration" "proxy_cors" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_method.proxy_cors.resource_id
-  http_method = aws_api_gateway_method.proxy_cors.http_method
-
-  type = "MOCK"
-
-  depends_on = [aws_api_gateway_method.proxy_cors]
-}
-
-resource "aws_api_gateway_method_response" "proxy_cors" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_method.proxy_cors.resource_id
-  http_method = aws_api_gateway_method.proxy_cors.http_method
-
-  status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-
-  depends_on = [aws_api_gateway_method.proxy_cors]
-}
-
-
-resource "aws_api_gateway_integration_response" "proxy_cors" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_method.proxy_cors.resource_id
-  http_method = aws_api_gateway_method.proxy_cors.http_method
-
-  status_code = aws_api_gateway_method_response.proxy_cors.status_code
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-
-  depends_on = [aws_api_gateway_method_response.proxy_cors]
-}
+#
+#resource "aws_api_gateway_method" "proxy_cors" {
+#  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+#  resource_id = aws_api_gateway_resource.proxy_resource.id
+#
+#  http_method   = "OPTIONS"
+#  authorization = "NONE"
+#}
+#
+#
+#resource "aws_api_gateway_integration" "proxy_cors" {
+#  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+#  resource_id = aws_api_gateway_method.proxy_cors.resource_id
+#  http_method = aws_api_gateway_method.proxy_cors.http_method
+#
+#  type = "MOCK"
+#
+#  depends_on = [aws_api_gateway_method.proxy_cors]
+#}
+#
+#resource "aws_api_gateway_method_response" "proxy_cors" {
+#  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+#  resource_id = aws_api_gateway_method.proxy_cors.resource_id
+#  http_method = aws_api_gateway_method.proxy_cors.http_method
+#
+#  status_code = "200"
+#
+#  response_models = {
+#    "application/json" = "Empty"
+#  }
+#
+#  response_parameters = {
+#    "method.response.header.Access-Control-Allow-Headers" = true,
+#    "method.response.header.Access-Control-Allow-Methods" = true,
+#    "method.response.header.Access-Control-Allow-Origin"  = true
+#  }
+#
+#  depends_on = [aws_api_gateway_method.proxy_cors]
+#}
+#
+#
+#resource "aws_api_gateway_integration_response" "proxy_cors" {
+#  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+#  resource_id = aws_api_gateway_method.proxy_cors.resource_id
+#  http_method = aws_api_gateway_method.proxy_cors.http_method
+#
+#  status_code = aws_api_gateway_method_response.proxy_cors.status_code
+#
+#  response_parameters = {
+#    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+#    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+#    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+#  }
+#
+#  depends_on = [aws_api_gateway_method_response.proxy_cors]
+#}
 
 #endregion
