7d6f0eb3be01427a49a5d15d0fb427556c47b993
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-17 16:16:54 -0700

    Change env

--- apps/svc-starter/infra/variables.tf
+++ apps/svc-starter/infra/variables.tf
@@ -13,7 +13,7 @@ variable "app_name" {
 variable "environment_name" {
   description = "The name of the deployment environment for your app, such as `dev` or `prod`"
   type        = string
-  default     = "dev2"
+  default     = "dev"
 }
 
 variable "lambda_runtime" {
