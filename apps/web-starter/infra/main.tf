ee2014869fd6a3f59e48ad69d8ffce7739134c8b
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-27 17:48:06 -0700

    Update main and outputs

--- apps/web-starter/infra/main.tf
+++ apps/web-starter/infra/main.tf
@@ -15,18 +15,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "frontend_bucket" {
-  source = "./modules/frontend-s3-bucket"
+module "frontend_dist" {
+  source = "./modules/frontend-serverless-dist"
 
   bucket_name    = "${var.app_name}-${var.environment_name}"
   dist_directory = "${path.module}/../../../dist/apps/${var.app_name}"
-}
-
-module "cf_s3_dist" {
-  source    = "./modules/cloudfront-s3-dist"
-  s3_bucket = {
-    id                          = module.frontend_bucket.bucket_id
-    arn                         = module.frontend_bucket.bucket_arn
-    bucket_regional_domain_name = module.frontend_bucket.bucket_regional_domain_name
-  }
 }
--- apps/web-starter/infra/outputs.tf
+++ apps/web-starter/infra/outputs.tf
@@ -1,4 +1,4 @@
 output "cloudfront_domain_name" {
   description = "The domain name to access the CloudFront distribution"
-  value       = module.cf_s3_dist.domain_name
+  value       = module.frontend_dist.cloudfront_domain_name
 }
